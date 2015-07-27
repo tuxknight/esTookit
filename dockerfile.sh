dfb5a80b9ec63f445470fb24e46a07e67cb5c7dc
Fuyuan.Chu <chufuyuan@foxmail.com>
2015-07-27 10:26:19 +0800

    change refs tree 4
    Change-Id: I796f0480d5ce3f7f5eec2fd9613738f1e357ee71

--- dockerfile.sh
+++ dockerfile.sh
@@ -2,5 +2,5 @@
 # render Dockerfile.template
 # git checkout -f $GERRIT_PATCHSET_REVISION
 FILE=./Dockerfile.template
-sed "s|RUN git fetch http://10.13.182.124:8081/flask-demo GERRIT_REFSPEC|RUN git fetch http://10.13.182.124:8081/flask-demo $GERRIT_REFSPEC" $FILE > ./Dockerfile
+sed "s|RUN git fetch http://10.13.182.124:8081/flask-demo GERRIT_REFSPEC|RUN git fetch http://10.13.182.124:8081/flask-demo $GERRIT_REFSPEC|g" $FILE > ./Dockerfile
 sed -i "s|RUN git checkout -f GERRIT_PATCHSET_REVISION|RUN git checkout -f $GERRIT_PATCHSET_REVISION|g" ./Dockerfile
