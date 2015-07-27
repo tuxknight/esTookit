273a108929402184cfa32b4d5c8663eeae86b9cd
Fuyuan.Chu <chufuyuan@foxmail.com>
2015-07-27 09:58:19 +0800

    checkout right refs3
    Change-Id: I5de5085b1260ea201c606c6be5d4110e5fd3ef6d

--- dockerfile.sh
+++ dockerfile.sh
@@ -2,4 +2,4 @@
 # render Dockerfile.template
 # git checkout -f $GERRIT_PATCHSET_REVISION
 FILE=./Dockerfile.template
-sed 's/RUN git checkout -f GERRIT_PATCHSET_REVISION/RUN git checkout -f $GERRIT_PATCHSET_REVISION/g' $FILE > ./Dockerfile
+sed "s/RUN git checkout -f GERRIT_PATCHSET_REVISION/RUN git checkout -f $GERRIT_PATCHSET_REVISION/g" $FILE > ./Dockerfile
